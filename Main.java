5fd2f227808c8d555198b0afa2f9560553e39701
Sergei <s.byduha@gmail.com>
2022-02-10 18:55:27 +0300

    Part 2 lesson 3

--- Main.java
+++ Main.java
@@ -1,47 +1,25 @@
-package ru.geekbrains.java2.lesson2;
+package ru.geekbrains.java2.lesson3;
+
+import java.util.*;
+
 
 public class Main {
-
-    static int SIZE = 4;
-
     public static void main(String[] args) {
-        String [][] ABC = {
-                {"1", "2", "5", "6", "9"},
-                {"3", "4", "7", "8"},
-                {"1", "2", "5", "6"},
-                {"3", "4", "7", "8"},
-        };
-        try {
-            System.out.print(sum (ABC));
-        } catch (MyArraySizeException m) {
-            m.printStackTrace();
-        } catch (MyArrayDataException m) {
-            m.printStackTrace();
-            System.out.println(m.getRow () + " " + m.getCol ());
-            System.out.println( ABC [m.getRow ()] [m.getCol ()]);
-        }
-    }
+        String word [] = {"Желтый", "Синий", "Красный" };
+        List <String> list = new ArrayList<>();
+        list.add("Зеленый");
+        System.out.println(list);
+        list.addAll(Arrays.asList (word));
+        System.out.println(list);
+        list.addAll(Arrays.asList ("Красный" , "Фиолетовый", "Желтый" , "Черный" , "Синий" , "Синий") );
+        System.out.println(list);
 
-    static int sum (String [][] ABC) throws  MyArrayDataException, MyArraySizeException {
-        if (ABC.length != SIZE) {
-            throw new MyArraySizeException();
+        Map<String, Integer> hmap = new HashMap<>();
+        for (String x : word) {
+            hmap.put(x, hmap.getOrDefault(x, 0) + 1);
         }
-        for (int i = 0; i < ABC.length; i++) {
-            if (ABC[i].length != SIZE) {
-                throw new MyArraySizeException();
-            }
-        }
-        int sum = 0;
-        for (int i = 0; i < ABC.length; i++) {
-            for (int j = 0; j < ABC.length; j++) {
-                try {
-                    sum += Integer.parseInt (ABC [i][j]);
-                } catch (NumberFormatException m) {
-                    throw new MyArrayDataException ("format" + i + " " + j, i, j);
-                }
-            }
-        }
-        return sum;
+        System.out.println(hmap);
+
     }
 
 }
