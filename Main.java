6a55d491b01569ddd78586b524c85d191c3fd5cb
Budyuhin <s.byduha@gmail.com>
2022-03-28 19:09:45 +0300

    Chatty2

--- Main.java
+++ Main.java
@@ -1,47 +1,23 @@
-package ru.geekbrains.java2.lesson2;
+package client;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    static int SIZE = 4;
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setTitle("Chatty");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-        String [][] ABC = {
-                {"1", "2", "5", "6", "9"},
-                {"3", "4", "7", "8"},
-                {"1", "2", "5", "6"},
-                {"3", "4", "7", "8"},
-        };
-        try {
-            System.out.print(sum (ABC));
-        } catch (MyArraySizeException m) {
-            m.printStackTrace();
-        } catch (MyArrayDataException m) {
-            m.printStackTrace();
-            System.out.println(m.getRow () + " " + m.getCol ());
-            System.out.println( ABC [m.getRow ()] [m.getCol ()]);
-        }
+        launch(args);
     }
-
-    static int sum (String [][] ABC) throws  MyArrayDataException, MyArraySizeException {
-        if (ABC.length != SIZE) {
-            throw new MyArraySizeException();
-        }
-        for (int i = 0; i < ABC.length; i++) {
-            if (ABC[i].length != SIZE) {
-                throw new MyArraySizeException();
-            }
-        }
-        int sum = 0;
-        for (int i = 0; i < ABC.length; i++) {
-            for (int j = 0; j < ABC.length; j++) {
-                try {
-                    sum += Integer.parseInt (ABC [i][j]);
-                } catch (NumberFormatException m) {
-                    throw new MyArrayDataException ("format" + i + " " + j, i, j);
-                }
-            }
-        }
-        return sum;
-    }
-
 }
